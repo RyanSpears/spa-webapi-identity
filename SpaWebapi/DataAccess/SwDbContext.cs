0944d612a9d0379c869c57fbe4883256ee216301
Ryan Spears <sperj001@hotmail.com>
2014-04-24 08:11:33 +0930

    Initial Commit

---
+++ SpaWebapi/DataAccess/SwDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using SpaWebapi.Models;
+
+namespace SpaWebapi.DataAccess
+{
+    public class SwDbContext : IdentityDbContext<User>
+    {
+        public SwDbContext()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ SpaWebapi/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using SpaWebapi.DataAccess;
+
+namespace SpaWebapi
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new SwDbContextInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
